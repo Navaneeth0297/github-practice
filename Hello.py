8310893fb74f6695f9229ad679f295f1d392d177
Navaneeth <nkv006@gmail.com>
2025-07-13 18:37:24 +0200

    update

--- Hello.py
+++ Hello.py
@@ -1,13 +1,3 @@
-def print_info(**kwargs):
-    if kwargs:  # Check if any keyword arguments were passed
-        print("\n--- Information ---")
-        for key, value in kwargs.items():
-            print(f"{key}: {value}")
-    else:
-        print("\nNo information provided.")
-
-# Example calls to the function
-print_info(name="Alice", age=30, city="New York")
-print_info(job="Engineer", salary=75000)
-print_info(country="USA", state="California", zip_code="90210")
-print_info() #loop
+lst = [1, 2, 3, 4, 5]
+list1=[ -1 if x%2!= 0 else x for x in lst ]
+print(list1)
