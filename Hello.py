21a5e0c73f9509c1f252e3fcf93fd84e23c36b80
Navaneeth <nkv006@gmail.com>
2025-07-13 09:10:54 +0200

    newupdate

--- Hello.py
+++ Hello.py
@@ -10,4 +10,4 @@ def print_info(**kwargs):
 print_info(name="Alice", age=30, city="New York")
 print_info(job="Engineer", salary=75000)
 print_info(country="USA", state="California", zip_code="90210")
-print_info()  # Call with no arguments
+print_info() 
